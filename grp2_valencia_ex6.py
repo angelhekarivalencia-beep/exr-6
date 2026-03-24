baf51242dc7ef61e422f48e55899c0f995214541
arvalencia@paterostechnologicalcollege.edu.ph <arvalencia@paterostechnologicalcollege.edu.ph>
2026-03-24 12:19:16 +0800

    Add script with conditionals and data structures

---
+++ grp2_valencia_ex6.py
@@ -0,0 +1,51 @@
+# Create a script that runs a condition in an IF statement
+
+age = 19
+
+print("Is age 18 or above?")
+
+print(age >= 18)
+
+
+
+if age >= 18:
+
+
+
+print("Condition is True")
+
+else:
+
+print("Condition is False")
+
+
+
+# Create a script that has List, Tuple, Set, and Dictionary
+
+
+my_list = ["Brown", 19, True]
+
+print("List:", my_list)
+
+
+my_tuple = ("Burger", 10, True)
+
+print("Tuple:", my_tuple)
+
+
+
+my_set = {"Milk", 7, False}
+
+print("Set:", my_set)
+
+
+
+my_dict = {
+
+"name": "Hekari",
+
+ }
+
+"score": 90
+
+print("Dictionary:", my_dict)|
